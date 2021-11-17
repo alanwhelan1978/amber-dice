7eab6d1e7479ceb82b1f5a0e21f588d02d7a5dba
alanwhelan1978 <alanwhelan1978@gmail.com>
2021-11-17 16:43:59 +0000

    Add quiz questions and correct annswers

---
+++ assets/js/script.js
@@ -0,0 +1,245 @@
+const questions = [
+    {
+        question: 'Which superhero has an assistant called Robin?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Batman' 
+    },
+
+    {
+        question: 'What does water turn into when it freezes?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Ice'
+    },
+
+    {
+        question: 'Which sport involves two small bats, a ball and a table?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Table tennis'
+    },
+
+    {
+        question: 'What country, beginning with the letter R, is the biggest country in the world'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Russia'
+    },
+
+    {
+        question: 'How many zeros are there in one hundred thousand?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Five'
+    },
+
+    {
+        question: 'Stockholm is the capital city of which country?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Sweden'
+    },
+
+    {
+        question: 'Can you unscramble the following word to reveal a number between one and ten: VNSEE'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Seven'
+    },
+
+    {
+        question: 'What is the definition of a habitat?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'The natural home of a animal'
+    },
+
+    {
+        question: 'Which country used picture writing many years ago called hieroglyphics?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Egypt' 
+    },
+
+    {
+        question: 'Who released a song in 2019 called "Hold Me While You Wait"?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Lewis Calpaldi'
+    },
+
+    {
+        question: 'How many colours are mentioned in the Rainbow Song?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '7'
+    },
+
+    {
+        question: 'What is half of 66?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '33'
+    },
+
+    {
+        question: 'Which continent does Great Britain belong to?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Europe'
+    },
+
+    {
+        question: 'How many points does a snowflake have?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '6'
+    },
+
+    {
+        question: 'What is the name of the largest desert in Asia?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Gobi'
+    },
+
+    {
+        question: 'What type of food do penguins eat?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Fish' 
+    },
+
+    {
+        question: 'As well as cheese, ham and tomato, what other topping is usually on a Hawaiian Pizza?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Pineapple' 
+    },
+
+    {
+        question: 'Who was the winner of The X Factor in 2015?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Louisa Johnson'
+    },
+
+    {
+        question: 'What is 11 + 111?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '122'
+    },
+
+    {
+        question: 'What flavour crisps are in a blue packet of Walkers?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Cheese and Onion'
+    },
+
+    {
+        question: 'How many days are there in January?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '31' 
+    },
+
+    {
+        question: 'What year did the Second World War end?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '1945'
+    },
+
+    {
+        question: 'What are incisors, canines, molars and premolars all types of?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Teeth'
+    },
+
+    {
+        question: 'What sugary fluid do bees remove from flowers to help them make honey?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Nectar'
+    },
+
+    {
+        question: 'Which male Disney character is able to fly and refuses to grow up?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: 'Peter Pan'
+    },
+
+    {
+        question: 'How many months of the year end with the letter Y?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '4'
+    },
+
+    {
+        question: 'How many years are there in one century?'
+        optionA: 
+        optionB: 
+        optionC: 
+        optionD: 
+        correctOption: '100'
+    }
+
+]
